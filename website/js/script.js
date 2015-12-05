6760dac12e41b45f944102e5d41e397f120f16c5
April Uzzle <april.uzzle@rochester.edu>
2015-12-05 01:32:49 -0500

    making site look pretty for testing

--- website/js/script.js
+++ website/js/script.js
@@ -1,47 +1,16 @@
-<html>
-
-<!--
-adapted from Lecture 11 and 12 code by Philip Guo for CSC 210
--->
-
-<head>
-<title>Motion Speak</title>
-
-
-
-</head>
-  <body>
-  <div id="home">
-  <h1>Welcome to Motion Speak!</h1>
-  <h2>Input ID Key: </h2>
-
-    <p>
-    ID Key: <input id="idKey" type="text" size="10"/>
-    </p>
-
-    <p>
-    <button id="submitButton">Submit</button>
-    </p> 
-    </div>  
-
-    <hr/>
-
-    <div id="contentArea" style="width: 100%; height: 60%">
-    </div>
-
-    <hr/>
-
-    <div id="transcription">
-    </div>
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="js/plotly-latest.min.js"></script>
-
-<script type="text/javascript">
-  $(document).ready(function() {
+$(document).ready(function() {
+  $("#content").hide();
+  $("#top").hide();
+  $("#nav").hide();
     localStorage.clear();
     localStorage["test"] = "work?";
 
     $("#submitButton").click(function(){
+      $("#intro").hide();
+      $("#top").show();
+      $("#content").show();
+      $("#nav").show();
+      $("#transcription").show();
       $.getJSON('data.json', function(data){
         var yVal = [];
         var xVal = [];
@@ -62,14 +31,24 @@ adapted from Lecture 11 and 12 code by Philip Guo for CSC 210
               type: 'scatter'
             }
           ];
-          $("#home").html("<h1>Here are your results</h1>");
-          $("#contentArea").html(Plotly.newPlot('contentArea', data2));
+          $("#top").html("<h2>Session Results: </h2><hr>");
+          $("#content").html(Plotly.newPlot('content', data2));
           $("#transcription").html("<h3>Transcription of Speech:</h3>\
             <p>The University of Rochester (commonly referred to as U of R or UR) is a private, nonsectarian, research university in Rochester, New York. The university grants undergraduate and graduate degrees, including doctoral and professional degrees. The university has six schools and various interdisciplinary programs.The University of Rochester is particularly noted for its Eastman School of Music. The university is also home to the Institute of Optics, founded in 1929, the first educational program in the US devoted exclusively to optics. Rochester's Laboratory for Laser Energetics is home to the second most energetic fusion laser in the world. In its history, five university alumni, two faculty, and one senior research associate at Strong Memorial Hospital have been awarded a Nobel Prize; eight alumni and four faculty members have won a Pulitzer Prize, and 19 faculty members have been awarded a Guggenheim Fellowship. Faculty and alumni of Rochester make up nearly one-quarter of the scientists on the board advising NASA in the development of the James Webb Space Telescope, which is scheduled to replace the Hubble Space Telescope in 2018.[citation needed] The departments of political science and economics have made a significant and consistent impact on positivist social science since the 1960s; the distinctive, mathematical approach pioneered at Rochester and closely affiliated departments is known as the Rochester school, and Rochester graduates and former affiliates are highly represented at faculties across top economics and political science departments.The University of Rochester, across all of its schools and campuses, enrolls approximately 5,600 undergraduates and 4,600 graduate students. Its 158 buildings house over 200 academic majors. Additionally, Rochester (along with its affiliated Strong Health System) is the largest employer in the Greater Rochester area and the sixth largest employer in New York.</p>");
         
       });
     });
+  $("#log").click(function(){
+    $.ajax({      
+      success: function(){
+        $("top").html("");
+        $("#content").html("");
+        $("#intro").show();
+        $("#content").hide();
+        $("#transcription").hide();
+        $("#nav").hide();
+        $("#top").hide();
+      }
+    })
   });
-</script>
-  </body>
-</html>
+  });
